9e330274debfc9608289ff5913d23e730eb01d0b
n1k0laj <n1k0lajby@gmail.com>
2013-03-26 15:00:53 +0400

    Initial commit

---
+++ alias.bash
@@ -0,0 +1,46 @@
+alias subl="~/bin/subl"
+alias schrome="open -a com.google.chrome --args --disable-web-security"
+alias b="/usr/bin/open -a '/Applications/Google Chrome.app'"
+
+alias e="vim"
+alias ecb="vim ~/.mybash/.bashrc"
+alias ecp="vim .bash_profile"
+alias eca="vim ~/.mybash/alias.bash"
+
+alias sserv="python -m SimpleHTTPServer"
+
+# Git 
+alias gs="git status"
+alias gc="git commit -m"
+alias ga="git add"
+alias gp="git push"
+alias gl="git log"
+
+# Sencha
+alias st="sencha -sdk ~/Downloads/touch-2.1.1 "
+alias stg="sencha generate"
+alias stgc="sencha generate controller"
+alias stgv="sencha generate view"
+alias stgm="sencha generate model"
+
+#list directory contents
+alias sl=ls
+alias la='ls -AF'       # Compact view, show hidden
+alias ls="ls -Ga"
+alias ll='ls -al'
+alias l='ls -a'
+alias l1='ls -1'
+
+alias ..='cd ..'         # Go up one directory
+alias ...='cd ../..'     # Go up two directories
+alias ....='cd ../../..' # Go up two directories
+alias -- -='cd -'        # Go back
+alias ~="cd ~"
+alias cdp="cd ~/Projects"
+
+#Shell History
+alias h='history'
+
+#Directory
+alias   md='mkdir -p'
+alias   rd='rmdir'
